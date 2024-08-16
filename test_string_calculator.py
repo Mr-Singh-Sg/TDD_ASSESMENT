3b404f147ea7462d0296692b573838cfa83684d2
Singh-Sg <deependrasg@gmail.com>
2024-08-16 15:50:40 +0530

    testcases for multiple and large numbers

--- test_string_calculator.py
+++ test_string_calculator.py
@@ -1,5 +1,6 @@
 import unittest
 from string_calculator import StringCalculator
+import random
 
 
 class TestStringCalculator(unittest.TestCase):
@@ -24,6 +25,22 @@ class TestStringCalculator(unittest.TestCase):
 
         result = self.calculator.add("1,2,3")  # passing three numbers
         self.assertEqual(1 + 2 + 3, result)
+
+    def test_add_any_amount_of_number(self):
+        """
+        Test cases for large and multiple numbers
+        """
+
+        result = self.calculator.add("10,20,30,40,50,60")
+        self.assertEqual(10 + 20 + 30 + 40 + 50 + 60, result)
+
+        result = self.calculator.add("111111,222222,333333,444444,555555")
+        self.assertEqual(111111 + 222222 + 333333 + 444444 + 555555, result)
+
+        data_list = [random.randrange(10000000000, 1000000000000) for _ in range(100)]
+        input_data = ", ".join([str(i) for i in data_list])
+        result = self.calculator.add(input_data)
+        self.assertEqual(sum(data_list), result)
 
 
 if __name__ == "__main__":
